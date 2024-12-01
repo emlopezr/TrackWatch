7e0ac601a0e0af67a8f8a0da68e8e0beb6d1297d
emlopezr <lopezrodemmanuel@gmail.com>
2024-12-01 15:59:47 -0500

    Delete POC - Update gitignore

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
-PLAYLIST_ID = os.getenv("SPOTIFY_PLAYLIST_ID")
