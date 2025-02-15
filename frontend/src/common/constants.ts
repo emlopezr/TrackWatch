fe9c6589d12f0b0ebfb18cbf08836893c76f8cf3
emlopezr <lopezrodemmanuel@gmail.com>
2025-02-14 21:45:48 -0500

    Use dinamyc variables

--- frontend/src/common/constants.ts
+++ frontend/src/common/constants.ts
@@ -1,10 +1,12 @@
-// Environment variables
+// Spotify App credentials
 export const SPOTIFY_CLIENT_ID  = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 export const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-export const SPOTIFY_REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+export const SPOTIFY_REDIRECT_URI = `${window.location.origin}/callback`;
 
 // Trackify Backend
-export const TRACKIFY_API_BASE_URL = import.meta.env.VITE_TRACKIFY_API_BASE_URL;
+export const TRACKIFY_API_BASE_URL = import.meta.env.VITE_TRACKIFY_API_BASE_URL.startsWith('http')
+  ? import.meta.env.VITE_TRACKIFY_API_BASE_URL
+  : `https://${import.meta.env.VITE_TRACKIFY_API_BASE_URL}`;
 
 // Spotify API
 export const SPOTIFY_BASE_URL = 'https://accounts.spotify.com';
