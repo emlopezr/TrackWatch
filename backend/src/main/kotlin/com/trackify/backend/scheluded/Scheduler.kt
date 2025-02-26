2852bcb231f0d73216e2618bb85d3f682c835981
emlopezr <lopezrodemmanuel@gmail.com>
2025-02-25 23:06:19 -0500

    Try to Schedule Controller method

--- backend/src/main/kotlin/com/trackify/backend/scheluded/Scheduler.kt
+++ backend/src/main/kotlin/com/trackify/backend/scheluded/Scheduler.kt
@@ -1,14 +1,19 @@
 package com.trackify.backend.scheluded
 
+import com.trackify.backend.controller.UtilsController
+import com.trackify.backend.utils.values.Constants
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
-class Scheduler(private val scheduledService: ScheduledService) {
+class Scheduler(private val utilsController: UtilsController) {
 
     @Scheduled(cron = "0 0 */4 * * *")
     fun runCoreTaskScheduled() {
-        scheduledService.runCoreTask()
+        utilsController.run(
+            System.getenv("ADMIN_KEY"),
+            Constants.DAYS_LIMIT
+        )
     }
 
 }
